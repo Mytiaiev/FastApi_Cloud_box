e3b39ed06ef2384805ac66023cb304683429e24c
Mytiaiev <mytiaievyevhen@gmail.com>
2022-12-20 11:07:28 +0200

    update modesl syntaxic

--- models/Files.py
+++ models/Files.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, String, Integer, select
-from fastapi_asyncalchemy.db.base import Base
+from .database import Base
 
 
 class Files(Base):
+    '''Class Files for Upload Files'''
     __tablename__ = "files"
 
     id = Column(Integer, autoincrement=True, primary_key=True, index=True)
     name = Column(String)
-    etag = Column(String, unique=True)
+    onject_id = Column(String, unique=True)
 
-    def get_all_files():
-        return select(Files).all()
+    def get_all_files(object: object) -> bool:
+        '''foo to check unique files in system for avoid duplicates '''
+        return True if object not in select(Files).all() else False
--- models/Users.py
+++ models/Users.py
@@ -1,14 +1,12 @@
-from sqlalchemy import Column, String, Integer, select
-from fastapi_asyncalchemy.db.base import Base
+from sqlalchemy import Column, String, Integer
+from .database import Base
 
 
 class Users(Base):
+    '''Cass for Users models.'''
     __tablename__ = "users"
 
     id = Column(Integer, autoincrement=True, primary_key=True, index=True)
     username = Column(String, unique=True)
     password = Column(String)
     token = Column(String, unique=True)
-
-    def get_all_users():
-        return select(Users).all()
